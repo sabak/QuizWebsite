d4228bb97a063838c4ef6b673e6672fa515bb0a7
sabak <s.kharabadze@gmail.com>
2016-06-08 01:36:51 +0400

    Imported files from previous repository

---
+++ src/models/Question.java
@@ -0,0 +1,18 @@
+package models;
+
+import java.util.ArrayList;
+
+/**
+ * Parent Class of all question-type classes
+ */
+public class Question {
+    private String question = "";
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
