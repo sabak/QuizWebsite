170aede7e0322a005268dfe0ad82cb32a0719ccb
sabak <s.kharabadze@gmail.com>
2016-06-29 19:15:33 +0400

    Added getQuiz implementation

--- src/ge/freeuni/quizwebsite/manager/dao/QuizManagerDAO.java
+++ src/ge/freeuni/quizwebsite/manager/dao/QuizManagerDAO.java
@@ -1,12 +1,11 @@
 package ge.freeuni.quizwebsite.manager.dao;
 
 import ge.freeuni.quizwebsite.manager.QuizManager;
-import ge.freeuni.quizwebsite.model.Account;
-import ge.freeuni.quizwebsite.model.Question;
-import ge.freeuni.quizwebsite.model.Quiz;
-import ge.freeuni.quizwebsite.model.QuizResult;
+import ge.freeuni.quizwebsite.manager.dao.db.DbContract;
+import ge.freeuni.quizwebsite.model.*;
 
 import javax.sql.DataSource;
+import java.sql.*;
 import java.util.List;
 
 /**
@@ -22,7 +21,34 @@ public class QuizManagerDAO extends AbstractManagerDAO implements QuizManager {
 
     @Override
     public Quiz getQuiz(Integer id) {
-        return null;
+        Quiz quiz = null;
+        try {
+            Connection con = dataSource.getConnection();
+            String query = "SELECT * FROM " + DbContract.Quiz.TABLE_NAME + " WHERE "
+                    + DbContract.Quiz.COLUMN_NAME_QUIZ_ID + " = ?;";
+            PreparedStatement statement = con.prepareStatement(query);
+            statement.setInt(1, id);
+            ResultSet rs = statement.executeQuery();
+
+            if (rs.next()) {
+                String name = rs.getString(DbContract.Quiz.COLUMN_NAME_NAME);
+                String description = rs.getString(DbContract.Quiz.COLUMN_NAME_DESCRIPTION);
+                String dateCreated = rs.getString(DbContract.Quiz.COLUMN_NAME_DATE_CREATED);
+                boolean hasRandomOrder = rs.getBoolean(DbContract.Quiz.COLUMN_NAME_HAS_RANDOM);
+                boolean isMultiplePage = rs.getBoolean(DbContract.Quiz.COLUMN_NAME_MULTIPLE_PAGE);
+                boolean isImmediatelyCorrected = rs.getBoolean(DbContract.Quiz.COLUMN_NAME_IMMEDIATE_CORRECTION);
+
+                quiz = new Quiz(id, name, description, hasRandomOrder,
+                        isImmediatelyCorrected, isMultiplePage ? PageType.MULTI_PAGE : PageType.ONE_PAGE,
+                        Timestamp.valueOf(dateCreated));
+            }
+            con.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return quiz;
     }
 
     @Override
