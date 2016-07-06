4aa2d0303d67bf17fdb423a2dc214a090dd41951
alex8912 <turdzeladze@gmail.com>
2016-07-06 07:17:15 +0400

    fixed loop on multiple page quizzes

--- src/ge/freeuni/quizwebsite/controller/servlet/GradeQuestion.java
+++ src/ge/freeuni/quizwebsite/controller/servlet/GradeQuestion.java
@@ -21,15 +21,16 @@ public class GradeQuestion extends HttpServlet {
         }
         session.setAttribute("index", i);
         String id = request.getParameter("Q_ID").substring(0, request.getParameter("Q_ID").length()-1);
-        System.out.println(id + " kadas");
         session.setAttribute("Q_ID", Integer.parseInt(id));
-        if(i == Integer.parseInt(request.getParameter("max").substring(0, request.getParameter("max").length()-1))){
+        Integer max = Integer.parseInt(request.getParameter("max").substring(0, request.getParameter("max").length()-1));
+        System.out.println(max + " bob");
+        if(i == max-1){
             RequestDispatcher rd = request.getRequestDispatcher("resultPage.jsp");
             rd.forward(request, response);
+        } else {
+            RequestDispatcher rd = request.getRequestDispatcher("multiplePageQuiz.jsp");
+            rd.forward(request, response);
         }
-
-        RequestDispatcher rd = request.getRequestDispatcher("multiplePageQuiz.jsp");
-        rd.forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
